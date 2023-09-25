32acbd4502b91795a7d2ebdaa64461a403fbc06b
nastia-h8 <96496878+nastia-h8@users.noreply.github.com>
2023-09-25 18:00:39 +0000

    Deploying to gh-pages from @ nastia-h8/goit-react-hw-05-movies@4db4a18cfb2cc53017ba30c652082fe31a6e365e 🚀

---
+++ static/js/692.b3a27b26.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[692],{7692:function(t,e,a){a.d(e,{rpH:function(){return c}});var r=a(9983);function c(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"m13 3 3.293 3.293-7 7 1.414 1.414 7-7L21 11V3z"}},{tag:"path",attr:{d:"M19 19H5V5h7l-2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5l-2-2v7z"}}]})(t)}}}]);
+//# sourceMappingURL=692.b3a27b26.chunk.js.map
