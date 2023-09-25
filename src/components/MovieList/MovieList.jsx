490313d1c337d9188fa5d2f0a1f2c7f3dfea6fee
Anastasiia Haliara <an.haliara@gmail.com>
2023-09-25 12:04:36 +0300

    add grid styles for movieList

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,21 +1,44 @@
 import PropTypes from 'prop-types';
 import { useLocation } from 'react-router-dom';
-import { List, StyledLink } from './MovieList.styled';
-import { AiFillPlayCircle } from 'react-icons/ai';
+import {
+  List,
+  StyledLink,
+  Item,
+  Wrapper,
+  Title,
+  ImgWrapper,
+  Img,
+} from './MovieList.styled';
+import placeholder from '../../images/placeholder.jpg';
 
 function MovieList({ movies, path = '' }) {
   const location = useLocation();
-
+  console.log(movies);
   return (
     <List>
-      {movies.map(({ id, title }) => (
-        <li key={id}>
-          <StyledLink to={`${path}${id}`} state={{ from: location }}>
-            <AiFillPlayCircle size={20} />
-            <span>{title}</span>
-          </StyledLink>
-        </li>
-      ))}
+      {movies.map(({ id, title, poster_path }) => {
+        return (
+          <Item key={id}>
+            <StyledLink to={`${path}${id}`} state={{ from: location }}>
+              <ImgWrapper>
+                <Img
+                  src={
+                    poster_path
+                      ? `https://image.tmdb.org/t/p/w300/${poster_path}`
+                      : `${placeholder}`
+                  }
+                  alt={title}
+                  width="250"
+                  height="200"
+                />
+              </ImgWrapper>
+              <Wrapper>
+                <Title>{title}</Title>
+              </Wrapper>
+            </StyledLink>
+          </Item>
+        );
+      })}
     </List>
   );
 }
--- src/components/MovieList/MovieList.styled.js
+++ src/components/MovieList/MovieList.styled.js
@@ -2,16 +2,30 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const List = styled.ul`
+  display: grid;
+  max-width: calc(100vw - 48px);
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-gap: ${p => p.theme.spacing(4)};
+  margin: 0 auto;
+`;
+
+export const Item = styled.li`
   position: relative;
-  display: flex;
-  flex-direction: column;
-  gap: ${p => p.theme.spacing(3)};
+  border-radius: ${p => p.theme.radii.sm};
+  box-shadow: ${p => p.theme.shadow};
+  overflow: hidden;
+  transition: transform ${p => p.theme.transition};
+
+  &:hover,
+  :focus {
+    transform: scale(0.98);
+  }
 `;
 
 export const StyledLink = styled(Link)`
   display: flex;
+  flex-direction: column;
   align-items: center;
-  gap: ${p => p.theme.spacing(3)};
   text-decoration: none;
   font-size: 16px;
   font-weight: 500;
@@ -28,4 +42,28 @@ export const StyledLink = styled(Link)`
   svg {
     fill: ${p => p.theme.colors.accent};
   }
+`;
+
+export const ImgWrapper = styled.div`
+  height: 320px;
+`;
+
+export const Img = styled.img`
+  object-fit: fill;
+  height: 100%;
+  overflow: hidden;
+`;
+
+export const Wrapper = styled.div`
+  padding: 16px 0;
+  min-height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.h2`
+  font-size: 18px;
+  line-height: 1.14;
+  letter-spacing: 0.02em;
 `;
